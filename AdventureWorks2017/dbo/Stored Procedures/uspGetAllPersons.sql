cea3db37bacd2e5237ef0aabd1fea35c5146c0c9
Pablo Gramajo <pdgramajo@gmail.com>
2018-05-05 19:41:13 -0300

    first commit

---
+++ AdventureWorks2017/dbo/Stored Procedures/uspGetAllPersons.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[uspGetAllPersons]
+AS
+	SELECT 
+		   BusinessEntityID
+		  ,PersonType
+		  ,NameStyle
+		  ,Title
+		  ,FirstName
+		  ,MiddleName
+		  ,LastName
+		  ,Suffix
+		  ,EmailPromotion
+		  ,AdditionalContactInfo
+		  ,Demographics
+		  ,rowguid
+		  ,ModifiedDate
+  FROM AdventureWorks2017.Person.Person
+RETURN 0
